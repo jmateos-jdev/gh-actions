4370a6d4135edfac46a729915020af58059b1afa
jmateos-jdev <joaquin.mateos@jdev.com.ar>
2024-04-30 11:02:25 -0300

    nueva rama con error

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.listen(8080, () => {
-  console.log("Server running on port 8080");
-});
+  console.log('Server running on port 8080')
+})
